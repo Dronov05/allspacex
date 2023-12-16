2a05f6ace640b29fc84fc72d749c0448e6e7ba1e
Max Dronov <23432-az@mail.ru>
2023-12-16 14:35:09 +0300

    Добавили фичу поиск пользовтелей
    [need build client]
    [need deploy server]

--- api/routes/all.js
+++ api/routes/all.js
@@ -4,8 +4,8 @@ const router = express.Router()
 
 router.all("*", (req, res, next) => {
 
-    // const domain = process.env.NODE_ENV === 'development' ? process.env.DEV_HOST : process.env.PROD_HOST // Для удалённого сервера
-    const domain = process.env.NODE_ENV === 'development' ? process.env.PROD_HOST : process.env.DEV_HOST // Для локального сервера
+    const domain = process.env.NODE_ENV === 'development' ? process.env.DEV_HOST : process.env.PROD_HOST // Для удалённого сервера
+    // const domain = process.env.NODE_ENV === 'development' ? process.env.PROD_HOST : process.env.DEV_HOST // Для локального сервера
 
     res.setHeader('Access-Control-Allow-Origin', domain)
     res.setHeader('Access-Control-Allow-Credentials', 'true')
