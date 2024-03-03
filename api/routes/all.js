87e17eecd3167caaa80d4d19ea5c3922e209c8d9
Max Dronov <23432-az@mail.ru>
2024-03-03 17:28:53 +0300

    Добавили фичу Добавление объявлений с заказами
    [need build client]
    [need deploy server]

--- api/routes/all.js
+++ api/routes/all.js
@@ -7,7 +7,6 @@ router.all("*", (req, res, next) => {
     const domain = process.env.NODE_ENV === 'development' ? process.env.DEV_HOST : process.env.PROD_HOST // Для удалённого сервера
     // const domain = process.env.NODE_ENV === 'development' ? process.env.PROD_HOST : process.env.DEV_HOST // Для локального сервера
 
-
     res.setHeader('Access-Control-Allow-Origin', domain)
     res.setHeader('Access-Control-Allow-Credentials', 'true')
     next()
